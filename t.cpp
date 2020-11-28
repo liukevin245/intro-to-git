124348bd0e591ef9773a059ae67ce214cb389984
liukevin245 <liukevin245@gmail.com>
2020-11-28 15:34:08 +0800

    finish age

--- t.cpp
+++ t.cpp
@@ -1,9 +1,19 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <string>
 using namespace std;
+
 int main(){
-	string name="";
-	cin >> name;
-	cout << name << endl;
-	return 0;
+    string name = "";
+    unsigned int age = 0;
+    cout << "please enter your name?" << endl;
+    cin >> name ;
+    
+    cout << "hello " << name << endl;
+    cout << "how old are you?" << endl;
+    cin >> age;
+    if(!age > 0)
+        age  = 0;
+    cout << "You are " << age << " year(s) old." << endl;
+    system("pause");
+    return 0;
 }
